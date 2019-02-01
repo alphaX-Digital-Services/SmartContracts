7b52d9f25fdf04e05d6abee743435cc56a1f71c1
DimaDevel <dima.ivanov.dev@gmail.com>
2019-02-01 13:45:08 +0200

    Create enum statuses and add action status for store job history and status.

--- eos/contracts/escrow/escrow.cpp
+++ eos/contracts/escrow/escrow.cpp
@@ -2,6 +2,9 @@
 
 namespace heymate {
 
+using std::string;
+using std::vector;
+
 ACTION escrow::create(
   uint64_t id,
   name client,
@@ -14,14 +17,14 @@ ACTION escrow::create(
   eosio_assert(is_account(client.value), "client account does not exist");
   eosio_assert(is_account(worker.value), "worker account does not exist");
 
-  eosio_assert(0 < escrow && escrow < 1000 , "escrow should be more than 0 and less than 1000");
-  eosio_assert(0 < reputation && reputation < 1000, "reputation should be more than 0 and less than 1000");
+  eosio_assert(0 < escrow && escrow <= 1000 , "escrow should be more than 0 and less then or equal to 1000");
+  eosio_assert(0 < reputation && reputation <= 1000, "reputation should be more than 0 and less then or equal to 1000");
 
   jobs_index jobs(_self, _self.value);
   auto found_job = jobs.find(id);
 
   eosio_assert(found_job == jobs.end(), "job with such id already exists");
-
+ 
   jobs.emplace(_self, [&](auto& job){
     job.id = id;
     job.client = client;
@@ -30,6 +33,7 @@ ACTION escrow::create(
     job.reputation = reputation;
     job.success = false;
     job.complete = false;
+    job.created = now();
   });
 
   //Call HMR burn for the worker
@@ -55,6 +59,22 @@ ACTION escrow::release(uint64_t id, uint64_t reputation)
 
   transfer_token(found_job.worker, found_job.escrow);
   mint_reputation(found_job.worker, reputation);
+}
+
+ACTION escrow::status(uint64_t id, string status, string history)
+{
+  require_auth(_self.value);
+  uint64_t statusNumber = convert(status);
+  eosio_assert(statusNumber, "undefined status");
+  jobs_index jobs(_self, _self.value);
+  const auto& found_job = jobs.get(id, "no job object found");
+  eosio_assert(!found_job.complete, "job is already completed");
+
+  jobs.modify(found_job, _self, [&](auto& job){
+    job.status = (char)statusNumber;
+    job.updated = now();
+    job.history.push_back(history);
+  });
 }
 
 ACTION escrow::refund(uint64_t id, uint64_t cancellationLogic)
@@ -87,6 +107,30 @@ ACTION escrow::refund(uint64_t id, uint64_t cancellationLogic)
 
 }
 
+statuses escrow::convert(const string& str)
+{
+  if(str == "proposal") return proposal;
+  else if(str == "acceptedProposal") return acceptedProposal;
+  else if(str == "dealDenied") return dealDenied;
+  else if(str == "started") return started;
+  else if(str == "createEscrowPending") return createEscrowPending;
+  else if(str == "escrowProcessed") return escrowProcessed;
+  else if(str == "suspended") return suspended;
+  else if(str == "completePending") return completePending;
+  else if(str == "completed") return completed;
+  else if(str == "applicationDenied") return applicationDenied;
+  else if(str == "workerDeliveryConfirmed") return workerDeliveryConfirmed;
+  else if(str == "clientDeliveryConfirmed") return clientDeliveryConfirmed;
+  else if(str == "clientFeedbackSet") return clientFeedbackSet;
+  else if(str == "failPending") return failPending;
+  else if(str == "failed") return failed;
+  else if(str == "archived") return archived;
+  else if(str == "workerDeclined") return workerDeclined;
+  else if(str == "clientWithdrawn")return clientWithdrawn;
+  else return undefined;
+
+}
+
 void escrow::transfer_token(name client, uint64_t escrow)
 {
   //Call HEY transfer back to the client
@@ -108,4 +152,4 @@ void escrow::mint_reputation(name worker, uint64_t amount)
 
 } /// namespace heymate
 
-EOSIO_DISPATCH(heymate::escrow, (create)(release)(refund))
+EOSIO_DISPATCH(heymate::escrow, (create)(release)(refund)(status))
--- eos/contracts/escrow/escrow.hpp
+++ eos/contracts/escrow/escrow.hpp
@@ -3,10 +3,38 @@
 #include <eosiolib/eosio.hpp>
 #include <eosiolib/asset.hpp>
 #include <eosiolib/symbol.hpp>
+#include <eosiolib/print.hpp>
+#include <eosiolib/system.h>
+#include <string>
 
 using namespace eosio;
 
 namespace heymate {
+
+  using std::string;
+  using std::vector;
+
+  enum statuses {
+    undefined = 0,
+    proposal,
+    acceptedProposal,
+    dealDenied,
+    started,
+    createEscrowPending,
+    escrowProcessed,
+    suspended,
+    completePending,
+    completed,
+    applicationDenied,
+    workerDeliveryConfirmed,
+    clientDeliveryConfirmed,
+    clientFeedbackSet,
+    failPending,
+    failed,
+    archived,
+    workerDeclined,
+    clientWithdrawn
+  } stat;
 
   CONTRACT escrow : public contract {
     using contract::contract;
@@ -24,6 +52,8 @@ namespace heymate {
 
       ACTION refund(uint64_t id, uint64_t cancellationLogic);
 
+      ACTION status(uint64_t id, string status, string history);
+
     private:
       TABLE job {
         uint64_t id;
@@ -31,15 +61,22 @@ namespace heymate {
         name worker;
         uint64_t escrow;
         uint64_t reputation;
+        char status;
         bool success;
         bool complete;
+        uint64_t created;
+        uint64_t updated;
+        vector<string> history;
 
         uint64_t primary_key() const { return id; }
+
+        EOSLIB_SERIALIZE( job, (id)(client)(worker)(escrow)(reputation)(status)(success)(complete)(created)(updated)(history) )
       };
 
       typedef multi_index<"jobs"_n, job> jobs_index;
 
       void transfer_token(name client, uint64_t escrow);
       void mint_reputation(name worker, uint64_t amount);
+      statuses convert(const string& str);
    };
 } /// namespace heymate
